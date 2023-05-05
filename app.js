c1042f70ed1235e00b1f723296fd846c8733fca5
Patryk <patrykwerbowy36@gmail.com>
2023-05-05 17:35:55 +0200

    wprowadznie nowego przycisku

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const button = document.getElementById("button");
 button.addEventListener("click", () => {
   alert("Hello world!");
 });
+const button = document.getElementById("Hi");
+button.addEventListener("click", () => {
+  alert("Hello universe!");
+});
