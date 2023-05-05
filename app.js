b5513bad5433732503d333c178f2f8b76606edf4
Patryk <patrykwerbowy36@gmail.com>
2023-05-05 17:38:39 +0200

    stworzenie nowego guzika

--- app.js
+++ app.js
@@ -2,7 +2,3 @@ const button = document.getElementById("button");
 button.addEventListener("click", () => {
   alert("Hello world!");
 });
-const button = document.getElementById("Hi");
-button.addEventListener("click", () => {
-  alert("Hello universe!");
-});
