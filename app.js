61403f7f51b14eebbc9964accd53fff8704e558d
Patryk <patrykwerbowy36@gmail.com>
2023-05-08 09:57:25 +0200

    usuniecie drugiego pliku js i wstawienie kodu do jednego

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const button = document.getElementById("button");
-button.addEventListener("click", () => {
+const buttton = document.getElementById("buttton");
+buttton.addEventListener("click", () => {
   alert("Hello world!");
+});
+const Hi = document.getElementById("Hi");
+Hi.addEventListener("click", () => {
+  alert("Hello universe!");
 });
--- apt.js
+++
@@ -1,4 +0,0 @@
-const Hi = document.getElementById("Hi");
-Hi.addEventListener("click", () => {
-  alert("Hello universe!");
-});
